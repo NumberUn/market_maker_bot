d3304106cfa24503b7f04af4f422d1a1d558cd43
HFTlab <arbitragecryptobotnet@gmail.com>
2024-02-12 12:00:00 +0100

    deleted collect after creating tasks

--- multi_bot.py
+++ multi_bot.py
@@ -167,7 +167,7 @@ class MultiBot:
         # deal['client_sell'].async_tasks.append(['create_order', sell_deal])
         ts_send = time.time()
         gc.unfreeze()
-        gc.collect()
+        # gc.collect()
         await asyncio.sleep(self.deal_pause)
         self.ap_deal_report(deal, client_id, precised_sz, ts_send)
         self.arbitrage_processing = False
