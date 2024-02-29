e32af1aa735d9b927a448f7f9f0d83fc6b853c37
HFTlab <arbitragecryptobotnet@gmail.com>
2024-02-29 15:04:30 +0100

    commented some parts of code

--- arbitrage_finder.py
+++ arbitrage_finder.py
@@ -145,10 +145,11 @@ class ArbitrageFinder:
                     sell_px = ob_sell['bids'][0][0]
                     raw_profit = (sell_px - buy_px) / buy_px
                     profit = raw_profit - self.fees[ex_buy] - self.fees[ex_sell]
+                    # name = f"B:{ex_buy}|S:{ex_sell}|C:{coin}"
+                    # print(f"{name} | Profit: {profit}")
                     if self.write_ranges:
                         name = f"B:{ex_buy}|S:{ex_sell}|C:{coin}"
                         self.append_profit(profit=raw_profit, name=name)
-                        # print(f"{name} | Profit: {profit}")
                         target_profit = self.target_profits.get(name, 'Not found')
                         if target_profit != 'Not found' and target_profit < 0 and direction == 'open':
                             continue
@@ -172,6 +173,7 @@ class ArbitrageFinder:
                     if profit >= target_profit:
                         if gc.isenabled():
                             gc.disable()
+                        # name = f"B:{ex_buy}|S:{ex_sell}|C:{coin}"
                         # print(f"TRIGGER: {trigger_exchange} {trigger_type} {name} PROFIT {profit}")
                         # print(f"BUY PX: {buy_px} | SELL PX: {sell_px} | DIRECTION: {direction}")
                         ts_buy, ts_sell = self.get_ob_pings(ob_buy, ob_sell)
