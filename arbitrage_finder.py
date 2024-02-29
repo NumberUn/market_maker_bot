047f9faa93a406d51a9a4211eb05bcf31ec6e9c6
HFTlab <arbitragecryptobotnet@gmail.com>
2024-02-29 16:10:37 +0100

    ping ws check added

--- arbitrage_finder.py
+++ arbitrage_finder.py
@@ -179,6 +179,7 @@ class ArbitrageFinder:
                         ts_buy, ts_sell = self.get_ob_pings(ob_buy, ob_sell)
                         if ts_buy > 0.15 or ts_sell > 0.15:
                             print(f"OB PING IS HUGE: {ts_sell=} {ts_buy=}")
+                            return
                         deal = {'client_buy': client_buy,
                                 'client_sell': client_sell,
                                 'buy_px': buy_px,
