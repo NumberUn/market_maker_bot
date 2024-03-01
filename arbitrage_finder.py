2326fa526089b71a6571a28c333a0bb494df1e27
HFTlab <arbitragecryptobotnet@gmail.com>
2024-03-01 12:37:06 +0100

    added prints on found AP

--- arbitrage_finder.py
+++ arbitrage_finder.py
@@ -173,12 +173,14 @@ class ArbitrageFinder:
                     if profit >= target_profit:
                         if gc.isenabled():
                             gc.disable()
-                        # name = f"B:{ex_buy}|S:{ex_sell}|C:{coin}"
-                        # print(f"TRIGGER: {trigger_exchange} {trigger_type} {name} PROFIT {profit}")
-                        # print(f"BUY PX: {buy_px} | SELL PX: {sell_px} | DIRECTION: {direction}")
+                        name = f"B:{ex_buy}|S:{ex_sell}|C:{coin}"
+                        print(f"TRIGGER: {trigger_exchange} {trigger_type} {name} PROFIT {profit}")
+                        print(f"BUY PX: {buy_px} | SELL PX: {sell_px} | DIRECTION: {direction}")
+                        print()
                         ts_buy, ts_sell = self.get_ob_pings(ob_buy, ob_sell)
                         if ts_buy > 0.15 or ts_sell > 0.15:
-                            # print(f"OB PING IS HUGE: {ts_sell=} {ts_buy=}")
+                            print(f"OB PING IS HUGE: {ts_sell=} {ts_buy=}")
+                            print()
                             return
                         deal = {'client_buy': client_buy,
                                 'client_sell': client_sell,
