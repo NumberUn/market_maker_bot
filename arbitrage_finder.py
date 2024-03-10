0bceeef0596c3d278f3515b9f665a0927445f42c
HFTlab <arbitragecryptobotnet@gmail.com>
2024-03-10 07:50:43 +0100

    changed limitations

--- arbitrage_finder.py
+++ arbitrage_finder.py
@@ -156,11 +156,11 @@ class ArbitrageFinder:
                             if not ob_sell.get('bids') or not ob_sell.get('asks'):
                                 continue
                             age_buy, age_sell = self.get_ob_ages(now_ts, ob_buy, ob_sell)
-                            if age_buy < 0.05:
-                                if age_sell < 0.05:
+                            if age_buy < 0.1:
+                                if age_sell < 0.1:
                                     ts_buy, ts_sell = self.get_ob_pings(ob_buy, ob_sell)
-                                    if ts_buy < 0.05:
-                                        if ts_sell < 0.05:
+                                    if ts_buy < 0.06:
+                                        if ts_sell < 0.06:
                                             # if not self.check_timestamps(client_buy, client_sell, ts_buy, ts_sell):
                                             #     continue
                                             poses = {x: y.get_positions() for x, y in self.clients_with_names.items()}
